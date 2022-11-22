406527906d1bc60012ed5c868b780fd68be76b1f
Pramudya Lutfi <lutfipramudya97@gmail.com>
2022-11-22 10:10:00 +0000

    Dev/pram

--- app/main/controller/category_controller.py
+++ app/main/controller/category_controller.py
@@ -1,13 +1,17 @@
+from app.main.service.category_service import get_all_category
 from flask_restx import Resource
 
 from app.main.api_model.category_am import CategoryApiModel
 
 category_ns = CategoryApiModel.api
+all_category_model = CategoryApiModel.all_category_model
 
 @category_ns.route("")
 class CategoriesController(Resource):
+    @category_ns.marshal_list_with(all_category_model, envelope="data")
     def get(self):
-        pass
+        return get_all_category()
+    
 
     def post(self):
         pass
