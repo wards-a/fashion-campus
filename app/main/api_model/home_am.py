fb60d43acf8d524eb37b205ac5a91386ac98278a
Mawardi Adama <mawardiadama@gmail.com>
2022-12-08 13:47:35 +0000

    home banner post n delete

--- app/main/api_model/home_am.py
+++ app/main/api_model/home_am.py
@@ -1,5 +1,5 @@
 from flask import url_for
-from flask_restx import Namespace, fields
+from flask_restx import Namespace, fields, reqparse
 
 
 class BannerImage(fields.Raw):
@@ -24,6 +24,9 @@ class CategoryImage(fields.Raw):
 class HomeApiModel:
     api = Namespace("home")
 
+    headers = reqparse.RequestParser()
+    headers.add_argument("Authentication", required=True, location="headers", help="Jwt-Token")
+
     home_category_model = api.model("HomeCategory", {
         "id": fields.String(description="uuid4"),
         "title": fields.String(attribute="name"),
@@ -34,3 +37,31 @@ class HomeApiModel:
         "title": fields.String(example="Winter sale banner"),
         "image": BannerImage(example="/image/winter-sale-banner.jpg")
     })
+
+    post_banner_schema = {
+        "type": "object",
+        "properties": {
+            "title": {
+                "type": "string",
+                "minLength": 1,
+                "errorMessage": {
+                    "required": "Title is required",
+                    "minLength": "Title cannot be null"
+                },
+                "example": "Special offer"
+            },
+            "image": {
+                "type": "string",
+                "minLength": 1,
+                "errorMessage": {
+                    "required": "Image is required",
+                    "minLength": "Image is required"
+                },
+                "example": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEBLAEsAAD/7gAOQWRvYmUAZAAAAAAA/+EQ3EV4aWYAAE1NACoAAAAIAAQBOwACAAAABgAACEqHaQAEAAAAAQAACFCcnQABAAAADAAAEMjqHAAAAv8Antd/99r/APE0Uf8ACNWf/PW4/wC+l/",
+                "description": "base64"
+            }
+        },
+        "required": ["title", "image"]
+    }
+
+    post_banner_model = api.schema_model("AddBanner", post_banner_schema)
--- app/main/service/home_service.py
+++ app/main/service/home_service.py
@@ -1,8 +1,14 @@
+import io
+
 from app.main import db
 from app.main.model.category import Category
 from app.main.model.banner import Banner
 from app.main.model.product import Product
 from app.main.model.product_image import ProductImage
+
+from app.main.utils.image_helper import secure_name, b64str_to_byte
+from app.main.utils.celery_tasks import upload_to_gcs, remove_from_gcs
+
 
 def get_home_categories():
     categories = db.session.execute(
@@ -32,4 +38,54 @@ def get_home_categories():
 def get_banner():
     result = db.session.execute(db.select(Banner).where(Banner.deleted=="0")).scalars().all()
     return result
+
+def save_new_banner(data):
+    new_banner = Banner(title=data['title'], image=data['title'])
+    try:
+        db.session.add(new_banner)
+        db.session.flush()
+        banner_name = _upload_banner(data)
+        new_banner.image = banner_name
+    except db.exc.IntegrityError as e:
+        db.session.rollback()
+        return {"message": "Banner could not be added", "error": str(e.orig)}, 400
     
+    db.session.commit()
+    return {"message": "Banner added"}, 201
+
+def delete_banner(id):
+    try:
+        image = db.session.execute(
+            db.delete(Banner)
+            .where(Banner.id==id)
+            .returning(Banner.image)
+        ).scalar()
+        remove_from_gcs.apply_async(args=[image], countdown=3)
+    except db.session.DataError as e:
+        return {"message": "Banner cannot be deleted", "error": str(e.orig)}, 400
+    
+    db.session.commit()
+    return {"message": "Banner deleted"}
+
+def _upload_banner(data):
+    b64_str = data['image']
+    title = secure_name(data['title'])
+
+    media_type = b64_str.split(',')[0]
+    media_type = media_type[media_type.find(':')+1 : media_type.find(';')]
+    extension = media_type.split('/')[1]
+    extension = 'jpg' if extension=='jpeg' else extension
+
+    result_byte = b64str_to_byte(b64_str)
+    
+    img_io = io.BytesIO(result_byte)
+    img_io.seek(0)
+    image = {
+        "filename": title+'.'+extension,
+        "media_type": media_type,
+        "file": img_io
+    }
+
+    upload_to_gcs.apply_async(args=[image], countdown=3)
+
+    return image['filename']
